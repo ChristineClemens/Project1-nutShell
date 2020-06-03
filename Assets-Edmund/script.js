4fdaac8bb713e201a1463f295deb3ef30e2f87b9
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-06-03 16:16:55 -0400

    Created sidebar

--- Assets-Edmund/script.js
+++ Assets-Edmund/script.js
@@ -1,8 +1,24 @@
+
+
+
+
 document.querySelector('#sidebar').addEventListener('click', function(){
+    const id = `${event.target.id}-row`
+
+    document.querySelectorAll('.list-group-item').forEach(value => {
+        value.setAttribute('style',`background-color: ${ (value.id == event.target.id)? '#999999':'transparent'} !important`)
+    })
+
+
     document.querySelectorAll(`.display-row`).forEach(value => {
-         const id = `${event.target.id}-row`
-         (value.id == id) ?
-        
-        )
+         value.style.display = (value.id == id) ? 'inline' : 'none'
+    })
     // document.querySelector(`#${event.target.id}-row`).style.display = 'inline';
+})
+
+document.querySelector('#sidebar').addEventListener('mouseover', function(){
+
+    document.querySelectorAll('.list-group-item').forEach(value => {
+        value.setAttribute('style',`background-color: ${ (value.id == event.target.id)? '#999999':'transparent'} !important`)
+    })
 })
