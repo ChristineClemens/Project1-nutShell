70ed822664abfa967a75834c3d218639a40ce2bf
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-06-03 12:11:14 -0400

    Initial Files

---
+++ Assets-Edmund/script.js
@@ -0,0 +1,8 @@
+document.querySelector('#sidebar').addEventListener('click', function(){
+    document.querySelectorAll(`.display-row`).forEach(value => {
+         const id = `${event.target.id}-row`
+         (value.id == id) ?
+        
+        )
+    // document.querySelector(`#${event.target.id}-row`).style.display = 'inline';
+})
