f8f0c4d5efb8ce9eb56e5189418c61286118038a
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-06-04 08:52:38 -0400

    Created theme color buttons

--- Assets-Edmund/script.js
+++ Assets-Edmund/script.js
@@ -1,6 +1,12 @@
 const sidebar = document.querySelector('#sidebar')
+const color = {
+    default: ['#666666','#999999','#cccccc','#333333'],
+    ponder: ['#0F4C81','#658DC6','#B5C7D3','#84898C'],
+    sunset: ['#FC8F9B','#E55982','#E881A6','#9D446E'],
+    calm:['#88B04B','#D4CACD','#C1CEC1','#EEA0A6']
+}
 
-
+let selectColor = 'default'
 
 sidebar.addEventListener('click', function(){
     sidebarItemColor(event.target.id)
@@ -20,6 +26,27 @@ sidebar.addEventListener('mouseover', function(){
 
 function sidebarItemColor(eventID){
     document.querySelectorAll('.list-group-item').forEach(value => {
-        value.setAttribute('style',`background-color: ${ (value.id == eventID)? '#999999':'transparent'} !important`)
+        value.setAttribute('style',`background-color: ${ (value.id == eventID)? color[selectColor][1] :'transparent'} !important`)
     })
+}
+
+function startup(){
+    themeChange(selectColor, false)
+}
+
+startup()
+
+document.querySelector('#theme').addEventListener('click',function(){
+    selectColor = event.target.id
+    themeChange(selectColor, true)
+
+    
+})
+
+function themeChange( id , isSetting){
+    document.querySelector('.navbar').setAttribute('style',`background-color: ${color[id][3]} !important`)
+    document.querySelector('.sidebar').setAttribute('style',`background-color: ${color[id][0]} !important`)
+    document.querySelector(`a${(isSetting)? '#setting':'#home'}`).setAttribute('style',`background-color: ${color[id][1]} !important`)
+
+
 }
