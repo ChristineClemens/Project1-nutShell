ea3df99f466367040ffe6972944e72e2f51d7f05
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-06-03 20:47:25 -0400

    Created responsive

--- Assets-Edmund/script.js
+++ Assets-Edmund/script.js
@@ -1,24 +1,25 @@
+const sidebar = document.querySelector('#sidebar')
 
 
 
-
-document.querySelector('#sidebar').addEventListener('click', function(){
-    const id = `${event.target.id}-row`
-
-    document.querySelectorAll('.list-group-item').forEach(value => {
-        value.setAttribute('style',`background-color: ${ (value.id == event.target.id)? '#999999':'transparent'} !important`)
-    })
-
+sidebar.addEventListener('click', function(){
+    sidebarItemColor(event.target.id)
 
     document.querySelectorAll(`.display-row`).forEach(value => {
-         value.style.display = (value.id == id) ? 'inline' : 'none'
+         value.style.display = (value.id == `${event.target.id}-row`) ? 'inline' : 'none'
     })
-    // document.querySelector(`#${event.target.id}-row`).style.display = 'inline';
 })
 
-document.querySelector('#sidebar').addEventListener('mouseover', function(){
 
+
+sidebar.addEventListener('mouseover', function(){
+    sidebarItemColor(event.target.id)
+})
+
+
+
+function sidebarItemColor(eventID){
     document.querySelectorAll('.list-group-item').forEach(value => {
-        value.setAttribute('style',`background-color: ${ (value.id == event.target.id)? '#999999':'transparent'} !important`)
+        value.setAttribute('style',`background-color: ${ (value.id == eventID)? '#999999':'transparent'} !important`)
     })
-})
+}
