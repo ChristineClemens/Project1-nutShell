bc16993f9aa8d64af0a9b6a8dca1d67df31261e4
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-06-05 09:12:42 -0400

    Created profile setting and logout button

--- Project1-Edmund/script.js
+++ Project1-Edmund/script.js
@@ -10,40 +10,43 @@ let selectColor = 'default'
 
 startup()
 
+//Loading Theme at Startup
 function startup(){
     if (!sessionStorage.email) window.location.href = './Project1-Shayanne/';
     else themeChange(selectColor, false)
 }
 
+//Sidebar Page onClick
 sidebar.addEventListener('click', function(){
     sidebarItemColor(event.target.id)
 
-    document.querySelectorAll(`.display-row`).forEach(value => {
-         value.style.display = (value.id == `${event.target.id}-row`) ? 'inline' : 'none'
-    })
+    if (event.target.id != 'logout'){
+        document.querySelectorAll(`.display-row`).forEach(value => {
+            value.style.display = (value.id == `${event.target.id}-row`) ? 'inline' : 'none'
+        })
+    }
 })
 
-
-
+//Sidebar Color Change MouseOver 
 sidebar.addEventListener('mouseover', function(){
     sidebarItemColor(event.target.id)
 })
 
 
-
+//Sidebar Color Change Function
 function sidebarItemColor(eventID){
     document.querySelectorAll('.list-group-item').forEach(value => {
         value.setAttribute('style',`background-color: ${ (value.id == eventID)? color[selectColor][1] :'transparent'} !important`)
     })
 }
 
-
+//Setting Theme Change onClick
 document.querySelector('#theme').addEventListener('click',function(){
     selectColor = event.target.id
     themeChange(selectColor, true)
 })
 
-
+//Setting Theme Change Function
 function themeChange( id , isSetting){
     if (isSetting) sidebarItemColor('setting')
     document.querySelector('.navbar').setAttribute('style',`background-color: ${color[id][3]} !important`)
@@ -55,9 +58,10 @@ function themeChange( id , isSetting){
     })
 
     metricChange(id)
+    buttonColor()
 }
 
-
+//Setting Weather Unit Change Function
 function metricChange(selectColor){
     const inactiveColor = color[selectColor][(selectColor == 'sunset')? 0 : 1]
     const activeColor = color[selectColor][(selectColor == 'sunset')? 1 : 0]
@@ -69,7 +73,43 @@ function metricChange(selectColor){
     document.querySelector('.active').setAttribute('style', `background-color: ${activeColor} !important; border-color: ${activeColor} !important;`)
 }
 
+//Setting Weather Unit Change onClick
 function changeUnit(){
     metricChange(selectColor)
 }
 
+//Button Color Change Function
+function buttonColor(){
+    document.querySelectorAll('.btn-color').forEach(button =>{
+        button.setAttribute('style',`background-color: ${color[selectColor][(selectColor == 'sunset')? 1 : 0]} !important`)
+        button.addEventListener('mouseover', function(){
+            button.setAttribute('style',`background-color: ${color[selectColor][(selectColor == 'sunset')? 0 : 1]} !important`)
+        })
+        button.addEventListener('mouseout', function(){
+            button.setAttribute('style',`background-color: ${color[selectColor][(selectColor == 'sunset')? 1 : 0]} !important`)
+        })
+    })
+}
+
+//Save Profile onClick
+document.querySelector('#saveProfile').addEventListener('click',function(){
+    const confirmPassword = document.querySelector('#confirmPassword')
+    const setPassword = document.querySelector('#setPassword').value
+
+    confirmPassword.setAttribute('class',`form-control ${(confirmPassword.value == setPassword)? '':'is-invalid'}`)
+})
+
+
+//Show Password Checkbox
+document.querySelector('#showPassword').addEventListener('change',function(){
+    const showPassword = (this.checked)? 'text':'password'
+    document.querySelector('#setPassword').setAttribute('type',showPassword)
+    document.querySelector('#confirmPassword').setAttribute('type',showPassword)
+})
+
+
+//Log Out onClick
+document.querySelector('#confirmLogout').addEventListener('click',function(){
+    sessionStorage.clear()
+    location.reload()
+})
